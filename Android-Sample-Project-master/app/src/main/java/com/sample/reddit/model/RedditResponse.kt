d91c923a758a509994e2825d5b9dd1d6936893df
Railan Xisto <railan.xisto@gmail.com>
2020-07-12 12:04:51 -0300

    Create models for api response and request

---
+++ Android-Sample-Project-master/app/src/main/java/com/sample/reddit/model/RedditResponse.kt
@@ -0,0 +1,25 @@
+package com.sample.reddit.model
+
+import com.squareup.moshi.Json
+import java.io.Serializable
+
+data class ApiResponse(
+    val kind: String?,
+    val data: Data?
+)
+
+data class Data(
+    val children: List<DataChildren>?,
+    val after: String?,
+    val before: String?
+)
+
+data class DataChildren(
+    @Json(name="data") val topic: Topic?
+)
+
+data class Topic(
+    val id: String,
+    val title: String?,
+    @Json(name="num_comments") val comments: Int = 0
+) : Serializable
