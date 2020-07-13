db4893f680d59cbee6a46776ca5e650388d0e67a
Railan Xisto <railan.xisto@gmail.com>
2020-07-13 00:24:07 -0300

    WIP - Experimenting State flow

--- Android-Sample-Project-master/app/src/main/java/com/sample/reddit/ui/list/ListFragment.kt
+++ Android-Sample-Project-master/app/src/main/java/com/sample/reddit/ui/list/ListFragment.kt
@@ -9,13 +9,14 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sample.reddit.databinding.ListFragmentBinding
 import com.sample.reddit.model.ApiResponse
-import com.sample.reddit.model.RequestParams
 import com.sample.reddit.model.Result
 import com.sample.reddit.ui.main.MainViewModel
-import com.sample.reddit.utils.*
 import kotlinx.android.synthetic.main.list_fragment.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class ListFragment : Fragment(), ListAdapter.TopicClickListener {
     private var _binding: ListFragmentBinding? = null
@@ -35,43 +36,47 @@ class ListFragment : Fragment(), ListAdapter.TopicClickListener {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        viewModel.requestArticles()
         setupAdapter()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.requestArticles(RequestParams())
-            .collectIn(lifecycleScope) {
-                    event ->
-                when (event) {
-                    is Start -> showLoading(true)
-                    is Success -> showSuccess(event.value)
-                    is Failure -> showError(event.exception)
-                    is Finish -> showLoading(false)
+        lifecycleScope.launch {
+            viewModel.state.collect { state ->
+                when (state) {
+                    is Result.Loading -> showLoading(true)
+                    is Result.Success -> showSuccess(state.content)
+                    is Result.Error -> showError(state.error)
                 }
             }
+        }
     }
 
     private fun setupAdapter() {
         val layoutManager = LinearLayoutManager(activity)
         topicsRecyclerView.layoutManager = layoutManager
         topicsRecyclerView.adapter = adapter
+
+        topicsRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (isBottom(dy, layoutManager)) {
+                    viewModel.requestMoreArticles()
+                }
+            }
+        })
     }
 
     private fun showError(exception: Throwable) {
+        showLoading(false)
         // TODO: implement error
         println("error")
     }
 
-    private fun showSuccess(result: Result<ApiResponse>) {
-        when (result) {
-            is Result.Success -> {
-                adapter.updateTopics(result.content.data?.children!!)
-            }
-            is Result.Error -> {
-                showError(result.error)
-            }
-        }
+    private fun showSuccess(result: ApiResponse) {
+        showLoading(false)
+        adapter.updateTopics(result.data?.children!!)
     }
 
     private fun showLoading(show: Boolean) {
@@ -85,5 +90,15 @@ class ListFragment : Fragment(), ListAdapter.TopicClickListener {
 
     override fun onTopicClick() {
         TODO("Not yet implemented")
+    }
+
+    fun isBottom(dy: Int, layoutManager: LinearLayoutManager): Boolean {
+        var isEndless = false
+        if (dy > 0) {
+            if (layoutManager.childCount + layoutManager.findFirstVisibleItemPosition() >= layoutManager.itemCount) {
+                isEndless = true
+            }
+        }
+        return isEndless
     }
 }
--- Android-Sample-Project-master/app/src/main/java/com/sample/reddit/ui/main/MainViewModel.kt
+++ Android-Sample-Project-master/app/src/main/java/com/sample/reddit/ui/main/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.sample.reddit.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sample.reddit.api.RedditRepository
 import com.sample.reddit.model.ApiResponse
 import com.sample.reddit.model.RequestParams
@@ -8,15 +9,41 @@ import kotlinx.coroutines.Dispatchers
 import com.sample.reddit.model.Result
 import com.sample.reddit.utils.StateMachine
 import com.sample.reddit.utils.StateMachineEvent
+import com.sample.reddit.utils.Success
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.switchMap
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     private val redditRepository: RedditRepository
 ) : ViewModel() {
+    var after: String? = null
+    @ExperimentalCoroutinesApi
+    private val _state = MutableStateFlow<Result<ApiResponse>>(
+        Result.Loading
+    )
 
-    fun requestArticles(params: RequestParams): Flow<StateMachineEvent<Result<ApiResponse>>> =
-        StateMachine(Dispatchers.IO) {
-            redditRepository.getSubreddit(params)
+
+    @ExperimentalCoroutinesApi
+    val state: StateFlow<Result<ApiResponse>> get() = _state
+
+    fun requestMoreArticles() =
+        requestArticles(RequestParams(after = after))
+
+    @ExperimentalCoroutinesApi
+    fun requestArticles(params: RequestParams = RequestParams()) {
+        viewModelScope.launch {
+            _state.value = Result.Loading
+            _state.value = withContext(Dispatchers.IO) {
+                redditRepository.getSubreddit(params)
+            }
         }
+
+    }
+
 }
